5b7ebfb93029dcd0c7287f55eddb125c4d7e17b8
Nate Johnson <nate@biobright.com>
2023-10-19 15:20:19 -0400

    Formatter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+use clap::{Arg, Command};
+use incr_stats::incr::Stats;
+use prettytable::{format, row, Cell, Row, Table};
 use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::os::unix::fs::MetadataExt;
-use std::path::{Path};
+use std::path::Path;
 use tree_magic;
-use prettytable::{Table, Row, Cell, row, format};
-use clap::{Arg, Command};
-use incr_stats::incr::Stats;
 
 // TODO: USE https://github.com/garyboone/incr_stats
 
@@ -14,14 +14,16 @@ fn main() {
         .version("0.1.0")
         .author("Your Name <your.email@example.com>")
         .about("Counts files by extension and displays MIME types")
-        .arg(Arg::new("DIRECTORY")
-            .help("Sets the working directory. Defaults to current directory if not set.")
-            .default_value(".")
-            .index(1))
+        .arg(
+            Arg::new("DIRECTORY")
+                .help("Sets the working directory. Defaults to current directory if not set.")
+                .default_value(".")
+                .index(1),
+        )
         .get_matches();
 
     //let working_dir = matches.value_of("DIRECTORY").unwrap_or(".");
-    let working_dir = matches.get_one::<String>("DIRECTORY").unwrap();//.unwrap_or(".");
+    let working_dir = matches.get_one::<String>("DIRECTORY").unwrap(); //.unwrap_or(".");
 
     // Recursively enumerate files in the specified directory
     let entries = fs::read_dir(&working_dir).expect("Failed to read directory");
@@ -31,7 +33,12 @@ fn main() {
 
     for entry in entries {
         if let Ok(entry) = entry {
-            process_entry(&entry.path(), &mut extension_counts, &mut extension_to_mime, &mut extension_to_stats);
+            process_entry(
+                &entry.path(),
+                &mut extension_counts,
+                &mut extension_to_mime,
+                &mut extension_to_stats,
+            );
         }
     }
 
@@ -46,7 +53,11 @@ fn main() {
             None => format!("{:?}", &HashSet::<String>::new()),
         };
         let stats_str = match extension_to_stats.get(ext) {
-            Some(stats_set) => format!("{:.2} ± {:.2}", (stats_set.mean().unwrap())/1024.0, (stats_set.sample_standard_deviation().unwrap_or(f64::NAN))/1024.0),
+            Some(stats_set) => format!(
+                "{:.2} ± {:.2}",
+                (stats_set.mean().unwrap()) / 1024.0,
+                (stats_set.sample_standard_deviation().unwrap_or(f64::NAN)) / 1024.0
+            ),
             None => format!("{:?}", ""),
         };
         table.add_row(Row::new(vec![
@@ -60,7 +71,12 @@ fn main() {
     table.printstd();
 }
 
-fn process_entry(path: &Path, counts: &mut HashMap<String, u32>, mime_map: &mut HashMap<String, HashSet<String>>, stats_map: &mut HashMap<String, Stats>) {
+fn process_entry(
+    path: &Path,
+    counts: &mut HashMap<String, u32>,
+    mime_map: &mut HashMap<String, HashSet<String>>,
+    stats_map: &mut HashMap<String, Stats>,
+) {
     // If it's a directory, recursively process its content
     if path.is_dir() {
         if let Ok(entries) = fs::read_dir(path) {
@@ -77,13 +93,19 @@ fn process_entry(path: &Path, counts: &mut HashMap<String, u32>, mime_map: &mut 
 
         // Determine MIME type using tree_magic
         let mime = tree_magic::from_filepath(path);
-        mime_map.entry(ext_str.clone()).or_insert_with(HashSet::new).insert(mime);
+        mime_map
+            .entry(ext_str.clone())
+            .or_insert_with(HashSet::new)
+            .insert(mime);
 
         let file_size = path.metadata().expect("file metadata call failed").size();
-        stats_map.entry(ext_str).or_insert_with(Stats::new).update(file_size as f64).unwrap();
+        stats_map
+            .entry(ext_str)
+            .or_insert_with(Stats::new)
+            .update(file_size as f64)
+            .unwrap();
 
-	   //println!("The skewness is {:.4}", stats_map.entry(ext_str).sample_skewness()?);
-       //println!("The kurtosis is {:.4}", stats_map.entry(ext_str).sample_kurtosis()?);
+        //println!("The skewness is {:.4}", stats_map.entry(ext_str).sample_skewness()?);
+        //println!("The kurtosis is {:.4}", stats_map.entry(ext_str).sample_kurtosis()?);
     }
 }
-
