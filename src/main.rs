fe36375586f3305eda2d7ee213b6294005b54e8a
Nate Johnson <nate@biobright.com>
2023-10-19 15:19:57 -0400

    Add size status

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::os::unix::fs::MetadataExt;
 use std::path::{Path};
 use tree_magic;
 use prettytable::{Table, Row, Cell, row, format};
 use clap::{Arg, Command};
+use incr_stats::incr::Stats;
 
 // TODO: USE https://github.com/garyboone/incr_stats
 
@@ -25,40 +27,46 @@ fn main() {
     let entries = fs::read_dir(&working_dir).expect("Failed to read directory");
     let mut extension_counts: HashMap<String, u32> = HashMap::new();
     let mut extension_to_mime: HashMap<String, HashSet<String>> = HashMap::new();
+    let mut extension_to_stats: HashMap<String, Stats> = HashMap::new();
 
     for entry in entries {
         if let Ok(entry) = entry {
-            process_entry(&entry.path(), &mut extension_counts, &mut extension_to_mime);
+            process_entry(&entry.path(), &mut extension_counts, &mut extension_to_mime, &mut extension_to_stats);
         }
     }
 
     // Create and print the table using prettytable
     let mut table = Table::new();
     table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-    table.set_titles(row!["Extension", "Count", "MIME Types"]);
+    table.set_titles(row!["Extension", "Count", "MIME Types", "Size (kb)"]);
 
     for (ext, count) in &extension_counts {
         let mime_str = match extension_to_mime.get(ext) {
             Some(mime_set) => format!("{:?}", mime_set),
             None => format!("{:?}", &HashSet::<String>::new()),
         };
+        let stats_str = match extension_to_stats.get(ext) {
+            Some(stats_set) => format!("{:.2} ± {:.2}", (stats_set.mean().unwrap())/1024.0, (stats_set.sample_standard_deviation().unwrap_or(f64::NAN))/1024.0),
+            None => format!("{:?}", ""),
+        };
         table.add_row(Row::new(vec![
             Cell::new(ext),
             Cell::new(&count.to_string()),
             Cell::new(&mime_str),
+            Cell::new(&stats_str),
         ]));
     }
 
     table.printstd();
 }
 
-fn process_entry(path: &Path, counts: &mut HashMap<String, u32>, mime_map: &mut HashMap<String, HashSet<String>>) {
+fn process_entry(path: &Path, counts: &mut HashMap<String, u32>, mime_map: &mut HashMap<String, HashSet<String>>, stats_map: &mut HashMap<String, Stats>) {
     // If it's a directory, recursively process its content
     if path.is_dir() {
         if let Ok(entries) = fs::read_dir(path) {
             for entry in entries {
                 if let Ok(entry) = entry {
-                    process_entry(&entry.path(), counts, mime_map);
+                    process_entry(&entry.path(), counts, mime_map, stats_map);
                 }
             }
         }
@@ -69,7 +77,13 @@ fn process_entry(path: &Path, counts: &mut HashMap<String, u32>, mime_map: &mut 
 
         // Determine MIME type using tree_magic
         let mime = tree_magic::from_filepath(path);
-        mime_map.entry(ext_str).or_insert_with(HashSet::new).insert(mime);
+        mime_map.entry(ext_str.clone()).or_insert_with(HashSet::new).insert(mime);
+
+        let file_size = path.metadata().expect("file metadata call failed").size();
+        stats_map.entry(ext_str).or_insert_with(Stats::new).update(file_size as f64).unwrap();
+
+	   //println!("The skewness is {:.4}", stats_map.entry(ext_str).sample_skewness()?);
+       //println!("The kurtosis is {:.4}", stats_map.entry(ext_str).sample_kurtosis()?);
     }
 }
 
